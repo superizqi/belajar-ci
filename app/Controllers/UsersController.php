5023fec52ff5b4c16997a16f8cde685338ab1c4f
superizqi <rizqiphd@gmail.com>
2020-09-07 20:10:45 +0700

    logout

--- app/Controllers/UsersController.php
+++ app/Controllers/UsersController.php
@@ -22,23 +22,38 @@ class UsersController extends BaseController
 			echo "data ada";
 			// ambil passwordnya lalu bandingkan
 			if ($password == $dataUser['password']) {
+				$session = session();
 				echo "password benar";
 				session()->setFlashdata('pesan','Sukses');
+				$newdata = [
+				        'username'  => $username,
+				        'logged_in' => TRUE
+				];
+				$session->set($newdata);
 				return redirect()->to('/UsersController');
 			}else{
 				echo "password salah";
 				// flashdata
+				$session->destroy();
 				session()->setFlashdata('pesan','Password salah');
 				return redirect()->to('/UsersController');
 			}
 
 		}else{
 			echo "data tdk ada";
+			$session->destroy();
 			session()->setFlashdata('pesan','Data tdk ada');
 			return redirect()->to('/login');
 		}
 
 		
+	}
+
+
+	public function logout(){
+		session()->destroy();
+		session()->setFlashdata('pesan','Logout Berhasil');
+		return redirect()->to('/UsersController');
 	}
 
 
--- app/Views/Pages/login.php
+++ app/Views/Pages/login.php
@@ -5,7 +5,12 @@
 <div class="container">
   <div class="row">
     <div class="col">
-      
+      <?php if (isset($_SESSION['username'])){
+        echo "Halo ".$_SESSION['username'];
+      }else{
+        echo "Kamu Belum Login";
+        }
+      ?>
       <?php if (session()->getFlashdata('pesan')) : ?>
         <div class="alert alert-success" role="alert">
           <?php echo session()->getFlashdata('pesan'); ?>
@@ -27,6 +32,8 @@
       </label>
       <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
     </form>
+      <br>
+      <a href="/UsersController/logout" class="btn btn-lg btn-primary btn-block" type="submit">Logout</a>   
 </div>
 
     </div>
